470ad2f9f479235e0c2d4828637ace29bf0db9c7
Harry9106 <33104677+Harry9106@users.noreply.github.com>
2017-10-25 18:29:30 -0300

    Update decode.vhd

--- DE0_NANO_mips/decode.vhd
+++ DE0_NANO_mips/decode.vhd
@@ -18,9 +18,9 @@ begin
   signextend : signext port map (a => Instr(15 downto 0), y => Signlmm);
 
   registers   : regfile port map (ra1 => Instr(25 downto 21),
-											 ra2 => Instr(20 downto 16),
-											 wa3 => WriteReg, wd3 => Result,
-											 we3 => RegWrite, clk => clk,
-											 rd1 => rd1, rd2 => rd2);
+		ra2 => Instr(20 downto 16),
+		wa3 => WriteReg, wd3 => Result,
+		we3 => RegWrite, clk => clk,
+		rd1 => rd1, rd2 => rd2);
 
 end architecture;
